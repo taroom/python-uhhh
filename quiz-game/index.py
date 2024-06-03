1c8503a6d4694f105ad6fc238ab451d3f7666874
Sultan Room <sultanroomcode@gmail.com>
2024-06-03 18:17:01 +0700

    add quiz question

--- quiz-game/index.py
+++ quiz-game/index.py
@@ -5,4 +5,17 @@ playing = input("Mau Main? ")
 if playing != "ya":
     quit()
 
-print("Anda ternyata main")
+print("Anda ternyata main! ")
+
+answer = input("Apa singkatan dari VS? ")
+if answer == "versus":
+    print("Anda benar")
+else:
+    print("Anda salah")
+
+# Ulangi Lagi
+answer = input("Apa singkatan dari tapera? ")
+if answer == "tabungan perumahan rakyat":
+    print("Anda benar")
+else:
+    print("Anda salah")
